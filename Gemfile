b43f1f43fb53c8e26fdcf0804503cce70e965cb9
Nicholas Sun <olassun2@gmail.com>
2014-06-29 20:43:57 -0400

    poking around and testing

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
 # app server
-gem 'thin', '1.6.2'
+gem 'thin'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+=begin
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,3 +59,4 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
+=end
