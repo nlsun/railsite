7efea7a08a19679e801e68d04d7dbb541950425d
Nicholas Sun <olassun2@gmail.com>
2014-06-30 01:56:51 -0400

    debug

--- config/initializers/environment_variables.rb
+++ config/initializers/environment_variables.rb
@@ -4,13 +4,13 @@ module EnvironmentVariablesExample
       env_file = Rails.root.join("config", 'environment_variables.yml').to_s
 
       if File.exists?(env_file)
-        puts "HI IM HERE"
-        puts YAML.load_file(env_file)[Rails.env]
+        #puts "HI IM HERE"
+        #puts YAML.load_file(env_file)[Rails.env]
         YAML.load_file(env_file)[Rails.env].each do |key, value|
-          puts 'the key'
-          puts key.to_s
-          puts 'the value'
-          puts value
+          #puts 'the key'
+          #puts key.to_s
+          #puts 'the value'
+          #puts value
           ENV[key.to_s] = value
         end # end YAML.load_file
       end # end if File.exists?
