463db28841db0585ddfa75129ea242a5ddd81bd8
Nicholas Sun <olassun2@gmail.com>
2014-07-16 01:25:22 -0400

    readded env var initialized

--- config/initializers/environment_variables.rb
+++ config/initializers/environment_variables.rb
@@ -1,16 +1,10 @@
-module EnvironmentVariablesExample
+module EnvironmentuariablesExample
   class Application < Rails::Application
     config.before_configuration do
       env_file = Rails.root.join("config", 'environment_variables.yml').to_s
 
       if File.exists?(env_file)
-        #puts "HI IM HERE"
-        #puts YAML.load_file(env_file)[Rails.env]
         YAML.load_file(env_file)[Rails.env].each do |key, value|
-          #puts 'the key'
-          #puts key.to_s
-          #puts 'the value'
-          #puts value
           ENV[key.to_s] = value
         end # end YAML.load_file
       end # end if File.exists?
