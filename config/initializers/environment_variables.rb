a4dcda1ec10cdc30b5547b274fefda849becac05
Nicholas Sun <olassun2@gmail.com>
2014-07-17 01:06:59 -0400

    added error check

--- config/initializers/environment_variables.rb
+++ config/initializers/environment_variables.rb
@@ -4,9 +4,11 @@ module EnvironmentvariablesExample
       env_file = Rails.root.join("config", 'environment_variables.yml').to_s
 
       if File.exists?(env_file)
-        YAML.load_file(env_file)[Rails.env].each do |key, value|
-          ENV[key.to_s] = value
-        end # end YAML.load_file
+        unless YAML.load_file(env_file)[Rails.env].empty?
+          YAML.load_file(env_file)[Rails.env].each do |key, value|
+            ENV[key.to_s] = value
+          end # end YAML.load_file
+        end
       end # end if File.exists?
     end # end config.before_configuration
   end # end class
